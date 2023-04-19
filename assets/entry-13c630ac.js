66d80d86a989ed603efe2cc3e220ff971cbbe158
Rosanchaudhary <chaudharyroshan2020@gmail.com>
2023-04-19 12:29:57 +0545

    second

---
+++ assets/entry-13c630ac.js
@@ -0,0 +1,1 @@
+window.onclick=function(o){const e=document.querySelector(".dropdown");!o.target.matches(".dropdown-toggle")&&!o.target.closest(".dropdown-menu")&&e.querySelector(".dropdown-menu.show")&&e.querySelector(".dropdown-menu.show").classList.remove("show")};
